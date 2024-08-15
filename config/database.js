d137eccdf3cf1c27b32f15ef954ade1ec6683202
Hamza El-Alamy <hamza.elalamy1@uit.ac.ma>
2024-08-15 14:54:08 +0100

    chore: Add database configuration and server setup

---
+++ config/database.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+// const DB = process.env.DB_URL ;
+const DB = "mongodb+srv://a36282303:gk74lc4cYOO5FZig@az.sn2laxp.mongodb.net/"
+
+mongoose.connect(`${DB}`)
+    .then(() => {
+        console.log(`Database connected successfully`);
+    })
+    .catch((err) => {
+        console.error('Error connecting to database:', err);
+    });
---
+++ index.js
@@ -0,0 +1,40 @@
+require('./config/database');
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const morgan = require('morgan');
+const path = require('path');
+
+const app = express();
+
+const port = process.env.PORT || 3500;
+
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+
+// app.use(express.static('public'));
+// app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+app.get('/', (req, res) => {
+    res.send('Testing!');
+});
+
+
+
+app.get("*", (req, res) => {
+    res.status(404).json({
+        message: "Page not found"
+    });
+});
+
+app.listen(port, () => {
+    console.log(`SERVER IS WORKING ON ${port}`);
+});
