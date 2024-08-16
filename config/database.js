f64483e58ca72ed5e9cc8514615f8964de9e2754
Hamza El-Alamy <hamza.elalamy1@uit.ac.ma>
2024-08-16 09:16:37 +0100

    refactor: Update database configuration to use environment variable

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
-// const DB = process.env.DB_URL ;
-const DB = "mongodb+srv://a36282303:gk74lc4cYOO5FZig@az.sn2laxp.mongodb.net/"
+const DB = process.env.DB_URL;
+// const DB = "mongodb+srv://a36282303:gk74lc4cYOO5FZig@az.sn2laxp.mongodb.net/"
 
 mongoose.connect(`${DB}`)
     .then(() => {
