c1af5bbbaaccc97bced2265e64853d1abc243f35
AYMAN-AZ-HUB <a36282303@gmail.com>
2024-08-16 14:24:28 +0100

    update postulation

--- routes/postulationRoutes.js
+++ routes/postulationRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const multer = require('multer');
+const {uploadpostulation} = require('../config/uploadpostulation');
 const router = express.Router();
 const postulationControllers = require('../controllers/postulationControllers');
 
-router.post('/application', postulationControllers.createPostulation);
+router.post('/application', uploadpostulation.single('cv'), postulationControllers.createPostulation);
 router.get('/application', postulationControllers.getPostulation);
 router.get('/application/:id', postulationControllers.getPostulationById);
 router.put('/application/:id', postulationControllers.putPostulation);
